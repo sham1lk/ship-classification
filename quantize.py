790d7fdd0d07bb5543a09d5af1aaaec2500a8cc3
Shamily <khastiev@gmail.com>
2023-12-26 11:10:37 +0300

    fix quantization

--- quantize.py
+++ quantize.py
@@ -35,11 +35,13 @@ def main(ckpt_path: str, save_to_onnx: bool):
     ans = trainer.predict(model, DataLoader(dataset, batch_size=256, shuffle=False, num_workers=127))
     ans = torch.cat(ans)
     ans = ans.numpy()
+    quantizer = Quantizer(ans)
     print(f"Number of predicted vectors: {len(ans)}")
-    print(f"Example of vector: {ans[0]}")
-    for i in range(ans.shape[1]):
-        print(f"quants for {i}:")
-        quantize_to_int8(ans[:, i])
+    print(f"Example of vector: {ans[42]}")
+    qvec = quantizer.quantize(ans[42])
+    print(f"Quantized: {qvec}")
+    print(f"Dequantized: {quantizer.dequantize(qvec)}")
+
 
 
 class Quant:
@@ -49,20 +51,41 @@ class Quant:
         self.value = value
 
     def __str__(self):
-        return str(float(self.value))
+        return f"upper: {str(float(self.upper))} lower: {str(float(self.lower))} value: {str(float(self.value))}"
+
+
+class Quantizer:
+    def __init__(self, ans: np.array):
+        quants = []
+        for i in range(ans.shape[1]):
+            print(f"quants for {i}:")
+            quants.append(quantize_to_int8(ans[:, i]))
+        self.quants = quants
+
+    def quantize(self, vec: np.ndarray):
+        new_arr = np.zeros(5)
+        for i, n in enumerate(vec):
+            for j, q in enumerate(self.quants[i]):
+                if q.lower <= n <= q.upper:
+                    new_arr[i] = j
+                    break
+        return new_arr
+
+    def dequantize(self, qvec: np.ndarray):
+        new_arr = np.zeros(5)
+        for i, n in enumerate(qvec):
+            new_arr[i] = self.quants[i][int(n)].value
+        return new_arr
+
 
 
 def quantize_to_int8(nums: np.ndarray):
     quants = []
     nums.sort()
-    # qsize = len(nums)//255
     qsize = (nums[-1] - nums[0]) / 255
     error = 0
 
     for i in range(254):
-    #     upper = nums[max(qsize * (i + 1) - 1, len(nums)-1)]
-    #     lower = nums[qsize * i]
-    #     value = (upper + lower) / 2
         q = nums[nums >= nums[0] + qsize * i]
         q = q[q <= nums[0] + qsize * (i + 1)]
         if not len(q):
@@ -76,7 +99,7 @@ def quantize_to_int8(nums: np.ndarray):
     print(f"Error: {error / len(nums)}")
     for i in range(10):
         print(str(quants[i]))
-
+    return quants
 
 
 if __name__ == "__main__":
